47685e318c3ec9c67c7b01556c7a258265e88b07
jiangliming <jiangliming@chelun.com>
2016-08-10 16:32:15 +0800

    New changelist

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,41 +1,183 @@
+const argv = require('yargs').argv;
+const autoprefixer = require('autoprefixer');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
-module.exports = {
-    entry: {
-        index: "./src/index.tsx",
-        react: "./src/react.tsx"
-    },
-    output: {
-        path: 'dist',
-        filename: '[name].bundle.js'
-    },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+//=========================================================
+//  ENVIRONMENT VARS
+//---------------------------------------------------------
+const NODE_ENV = process.env.NODE_ENV;
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js", ".scss"]
-    },
+const ENV_DEVELOPMENT = NODE_ENV === 'development';
+const ENV_PRODUCTION = NODE_ENV === 'production';
+const ENV_TEST = NODE_ENV === 'test';
 
-    module: {
-        loaders: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-            {test: /\.tsx?$/, loader: "ts-loader?modules&localIdentName=[name]__[local]-[hash:base64:5]"},
-            {test: /\.scss$/, loader: "style!css!autoprefixer-loader?browsers=last 2 versions!sass", exclude: /node_modules/}
-        ],
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {test: /\.css$/, loader: "source-map-loader"}
-        ]
-    },
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
 
-// When importing a module whose path matches one of the following, just
-// assume a corresponding global variable exists and use that instead.
-// This is important because it allows us to avoid bundling all of our
-// dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        // "react": "React",
-        // "react-dom": "ReactDOM"
-    },
+
+//=========================================================
+//  CONFIG
+//---------------------------------------------------------
+const config = {};
+module.exports = config;
+
+
+config.resolve = {
+    extensions: ['', '.ts', '.js'],
+    modulesDirectories: ['node_modules'],
+    root: path.resolve('.')
 };
+
+config.module = {
+    loaders: [
+        {test: /\.ts$/, loader: 'ts', exclude: /node_modules/},
+        {test: /\.html$/, loader: 'raw'},
+        {test: /\.scss$/, loader: 'raw!postcss!sass', exclude: path.resolve('src/views/common/styles'), include: path.resolve('src/views')}
+    ]
+};
+
+config.plugins = [
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+    })
+];
+
+config.postcss = [
+    autoprefixer({ browsers: ['last 3 versions'] })
+];
+
+config.sassLoader = {
+    outputStyle: 'compressed',
+    precision: 10,
+    sourceComments: false
+};
+
+
+//=====================================
+//  DEVELOPMENT or PRODUCTION
+//-------------------------------------
+if (ENV_DEVELOPMENT || ENV_PRODUCTION) {
+    config.entry = {
+        main: ['./src/main.ts'],
+        polyfills: './src/polyfills.ts',
+        vendor: './src/vendor.ts'
+    };
+
+    config.output = {
+        filename: '[name].js',
+        path: path.resolve('./target'),
+        publicPath: '/'
+    };
+
+    config.plugins.push(
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['vendor', 'polyfills'],
+            minChunks: Infinity
+        }),
+        new CopyWebpackPlugin([
+            {from: './src/assets', to: 'assets'}
+        ]),
+        new HtmlWebpackPlugin({
+            chunkSortMode: 'dependency',
+            filename: 'index.html',
+            hash: false,
+            inject: 'body',
+            template: './src/index.html'
+        })
+    );
+}
+
+
+//=====================================
+//  DEVELOPMENT
+//-------------------------------------
+if (ENV_DEVELOPMENT) {
+    config.devtool = 'cheap-module-source-map';
+
+    config.entry.main.unshift(`webpack-dev-server/client?http://${HOST}:${PORT}`);
+
+    config.module.loaders.push(
+        {test: /\.scss$/, loader: 'style!css!postcss!sass', include: path.resolve('src/views/common/styles')}
+    );
+
+    config.devServer = {
+        contentBase: './src',
+        historyApiFallback: true,
+        host: HOST,
+        outputPath: config.output.path,
+        port: PORT,
+        publicPath: config.output.publicPath,
+        stats: {
+            cached: true,
+            cachedAssets: true,
+            chunks: true,
+            chunkModules: false,
+            colors: true,
+            hash: false,
+            reasons: true,
+            timings: true,
+            version: false
+        }
+    };
+}
+
+
+//=====================================
+//  PRODUCTION
+//-------------------------------------
+if (ENV_PRODUCTION) {
+    config.devtool = 'source-map';
+
+    config.output.filename = '[name].[chunkhash].js';
+
+    config.module.loaders.push(
+        {test: /\.scss$/, loader: ExtractTextPlugin.extract('css?-autoprefixer!postcss!sass'), include: path.resolve('src/views/common/styles')}
+    );
+
+    config.plugins.push(
+        new WebpackMd5Hash(),
+        new ExtractTextPlugin('styles.[contenthash].css'),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: true,
+            compress: {
+                dead_code: true, // eslint-disable-line camelcase
+                screw_ie8: true, // eslint-disable-line camelcase
+                unused: true,
+                warnings: false
+            }
+        })
+    );
+}
+
+
+//=====================================
+//  TEST
+//-------------------------------------
+if (ENV_TEST) {
+    config.devtool = 'inline-source-map';
+
+    config.module.loaders.push(
+        {test: /\.scss$/, loader: 'style!css!postcss!sass', include: path.resolve('src/views/common/styles')}
+    );
+
+    if (argv.coverage) {
+        config.module.postLoaders = [
+            {
+                test: /\.(js|ts)$/,
+                loader: 'istanbul-instrumenter-loader',
+                include: path.resolve('./src'),
+                exclude: [
+                    /\.spec\.ts$/,
+                    /node_modules/
+                ]
+            }
+        ];
+    }
+}
