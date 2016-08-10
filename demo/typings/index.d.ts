aec5b9f72ac516c39c1f51c54642c9c6c0ddf788
jiangliming <jiangliming@chelun.com>
2016-08-10 16:31:43 +0800

    update

--- demo/typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/backbone/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
-/// <reference path="globals/underscore/index.d.ts" />
