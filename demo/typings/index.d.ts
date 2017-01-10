b74753ceb30fffa22d50556e7f451d1a5d8d768b
ar.insect <ar.insect@gmail.com>
2017-01-10 16:53:21 +0800

    update

---
+++ demo/typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/require/index.d.ts" />
+/// <reference path="globals/underscore/index.d.ts" />
+/// <reference path="modules/backbone/index.d.ts" />
